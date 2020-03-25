046b54061c0a515b129abbd220334a420436ac12
Daniel D <doubledd99@outlook.com>
2020-03-25 16:36:49 -0500

    done

--- Lab 4/main.cpp
+++ Lab 4/main.cpp
@@ -7,6 +7,9 @@ Lab 4 - Recursion
 
 #include "Stack.h" //includes stack class from lab 3
 
+//Precondition: two stacks have to be created
+//Postcondtion: the old stack is empty and new stack contains all nodes from old stack in reversed order
+
 void reverse(Stack list1, Stack Flipped); //void function prototype
 int main() { //main function
 	Stack list1; //creates a stack to insert items in the "correct" order
@@ -22,9 +25,9 @@ int main() { //main function
 }
 void reverse(Stack list1, Stack Flipped) {//void function definition that inputs the stack with information already and the stack that is empty as a template
 	Flipped.push(list1.top()); //adds the top item from the old stack to the bottom of the new stack, reversing its order
-	list1.pop();
-	cout << Flipped.top() << endl;
-	if (!list1.isEmpty()) {
-		reverse(list1, Flipped);
+	list1.pop(); //removes the top item on the old stack to be reversed exposing the next node
+	cout << Flipped.top() << endl; //displays the new stack's order one int per line
+	if (!list1.isEmpty()) { //as long as the "old" stack isnt empty it will call itself recursively
+		reverse(list1, Flipped); //recursive function call of itself inputting both stacks.
 	}
 }
